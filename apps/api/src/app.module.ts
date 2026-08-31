543f40b9847f85c840156274f8715887a4e05455
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-08-31 19:52:47 +0300

    fix: register HR module in API application

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -14,6 +14,7 @@ import { PaymentsModule } from './modules/payments/payments.module';
 import { RolesModule } from './modules/roles/roles.module';
 import { MembershipsModule } from './modules/memberships/memberships.module';
 import { InventoryModule } from './modules/inventory/inventory.module';
+import { HrModule } from './modules/hr/hr.module';
 
 @Module({
   imports: [
@@ -31,6 +32,7 @@ import { InventoryModule } from './modules/inventory/inventory.module';
     RolesModule,
     MembershipsModule,
     InventoryModule,
+    HrModule,
   ],
 })
 export class AppModule {}
