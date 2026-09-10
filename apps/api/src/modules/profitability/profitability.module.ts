d8c627ddd574a77e6525375f2fc3491d4aed19e9
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-10 11:37:59 +0300

    feat(reporting): register net profitability services

--- apps/api/src/modules/profitability/profitability.module.ts
+++ apps/api/src/modules/profitability/profitability.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProfitabilityController } from './profitability.controller';
 import { ProfitabilityService } from './profitability.service';
+import { NetProfitabilityService } from './net-profitability.service';
+import { ProfitabilityConfigService } from './profitability-config.service';
 
 @Module({
   controllers: [ProfitabilityController],
-  providers: [ProfitabilityService],
-  exports: [ProfitabilityService],
+  providers: [ProfitabilityService, NetProfitabilityService, ProfitabilityConfigService],
+  exports: [ProfitabilityService, NetProfitabilityService, ProfitabilityConfigService],
 })
 export class ProfitabilityModule {}
