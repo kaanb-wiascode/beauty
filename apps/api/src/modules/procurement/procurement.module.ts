c974a04adf0a87cbe3ad678b3f6389f281fee0b1
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-10 16:41:24 +0300

    feat(procurement): register return approval workflow

--- apps/api/src/modules/procurement/procurement.module.ts
+++ apps/api/src/modules/procurement/procurement.module.ts
@@ -4,10 +4,23 @@ import { ProcurementService } from './procurement.service';
 import { ProcurementRequestsService } from './procurement-requests.service';
 import { ProcurementApprovalsService } from './procurement-approvals.service';
 import { ProcurementReturnsService } from './procurement-returns.service';
+import { ProcurementReturnRequestsService } from './procurement-return-requests.service';
 
 @Module({
   controllers: [ProcurementController],
-  providers: [ProcurementService, ProcurementRequestsService, ProcurementApprovalsService, ProcurementReturnsService],
-  exports: [ProcurementService, ProcurementRequestsService, ProcurementApprovalsService, ProcurementReturnsService],
+  providers: [
+    ProcurementService,
+    ProcurementRequestsService,
+    ProcurementApprovalsService,
+    ProcurementReturnsService,
+    ProcurementReturnRequestsService,
+  ],
+  exports: [
+    ProcurementService,
+    ProcurementRequestsService,
+    ProcurementApprovalsService,
+    ProcurementReturnsService,
+    ProcurementReturnRequestsService,
+  ],
 })
 export class ProcurementModule {}
