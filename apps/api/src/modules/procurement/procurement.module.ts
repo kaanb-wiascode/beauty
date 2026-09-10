2a0d26030d82d9aad3093947d4933d36025b7fa5
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-10 11:26:30 +0300

    feat(procurement): register purchase request workflow service

--- apps/api/src/modules/procurement/procurement.module.ts
+++ apps/api/src/modules/procurement/procurement.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProcurementController } from './procurement.controller';
 import { ProcurementService } from './procurement.service';
+import { ProcurementRequestsService } from './procurement-requests.service';
 
 @Module({
   controllers: [ProcurementController],
-  providers: [ProcurementService],
-  exports: [ProcurementService],
+  providers: [ProcurementService, ProcurementRequestsService],
+  exports: [ProcurementService, ProcurementRequestsService],
 })
 export class ProcurementModule {}
