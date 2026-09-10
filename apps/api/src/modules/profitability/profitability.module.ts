8e3b0c0ee2729adcf8fcbb194668a28e3d746879
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-10 11:47:41 +0300

    feat(finance): register budgeting service

--- apps/api/src/modules/profitability/profitability.module.ts
+++ apps/api/src/modules/profitability/profitability.module.ts
@@ -4,10 +4,23 @@ import { ProfitabilityService } from './profitability.service';
 import { NetProfitabilityService } from './net-profitability.service';
 import { ProfitabilityConfigService } from './profitability-config.service';
 import { CostCenterService } from './cost-center.service';
+import { BudgetingService } from './budgeting.service';
 
 @Module({
   controllers: [ProfitabilityController],
-  providers: [ProfitabilityService, NetProfitabilityService, ProfitabilityConfigService, CostCenterService],
-  exports: [ProfitabilityService, NetProfitabilityService, ProfitabilityConfigService, CostCenterService],
+  providers: [
+    ProfitabilityService,
+    NetProfitabilityService,
+    ProfitabilityConfigService,
+    CostCenterService,
+    BudgetingService,
+  ],
+  exports: [
+    ProfitabilityService,
+    NetProfitabilityService,
+    ProfitabilityConfigService,
+    CostCenterService,
+    BudgetingService,
+  ],
 })
 export class ProfitabilityModule {}
