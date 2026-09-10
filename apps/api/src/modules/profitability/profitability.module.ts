1b9cf8dce2ccb55ff56056b91269ac49a13bd54e
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-10 12:01:52 +0300

    feat(finance): register treasury risk service

--- apps/api/src/modules/profitability/profitability.module.ts
+++ apps/api/src/modules/profitability/profitability.module.ts
@@ -6,6 +6,7 @@ import { ProfitabilityConfigService } from './profitability-config.service';
 import { CostCenterService } from './cost-center.service';
 import { BudgetingService } from './budgeting.service';
 import { CashFlowForecastService } from './cash-flow-forecast.service';
+import { TreasuryRiskService } from './treasury-risk.service';
 
 @Module({
   controllers: [ProfitabilityController],
@@ -16,6 +17,7 @@ import { CashFlowForecastService } from './cash-flow-forecast.service';
     CostCenterService,
     BudgetingService,
     CashFlowForecastService,
+    TreasuryRiskService,
   ],
   exports: [
     ProfitabilityService,
@@ -24,6 +26,7 @@ import { CashFlowForecastService } from './cash-flow-forecast.service';
     CostCenterService,
     BudgetingService,
     CashFlowForecastService,
+    TreasuryRiskService,
   ],
 })
 export class ProfitabilityModule {}
