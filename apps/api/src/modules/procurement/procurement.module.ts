1586e31d445163d97c0deef32949a27b3c86940d
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-10 11:31:33 +0300

    feat(procurement): register purchase order approvals

--- apps/api/src/modules/procurement/procurement.module.ts
+++ apps/api/src/modules/procurement/procurement.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProcurementController } from './procurement.controller';
 import { ProcurementService } from './procurement.service';
 import { ProcurementRequestsService } from './procurement-requests.service';
+import { ProcurementApprovalsService } from './procurement-approvals.service';
 
 @Module({
   controllers: [ProcurementController],
-  providers: [ProcurementService, ProcurementRequestsService],
-  exports: [ProcurementService, ProcurementRequestsService],
+  providers: [ProcurementService, ProcurementRequestsService, ProcurementApprovalsService],
+  exports: [ProcurementService, ProcurementRequestsService, ProcurementApprovalsService],
 })
 export class ProcurementModule {}
