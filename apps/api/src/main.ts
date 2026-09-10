57b70212c2fa3bfa948994e09aaa8f269a7fbe68
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-10 09:21:00 +0300

    fix(api): avoid undeclared validation runtime dependency

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,5 +1,4 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { AppLoggerService } from './common/logging/app-logger.service';
 import { HttpExceptionFilter } from './common/http/http-exception.filter';
@@ -8,17 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.useLogger(app.get(AppLoggerService));
   app.useGlobalFilters(new HttpExceptionFilter());
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      validationError: {
-        target: false,
-        value: false,
-      },
-    }),
-  );
 
   app.enableCors({
     origin: [
