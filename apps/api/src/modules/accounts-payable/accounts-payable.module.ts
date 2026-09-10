763c2269244be37ee4218f61d3064a4ad358b110
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-10 16:39:07 +0300

    feat(ap): register credit-note analytics

--- apps/api/src/modules/accounts-payable/accounts-payable.module.ts
+++ apps/api/src/modules/accounts-payable/accounts-payable.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AccountsPayableController } from './accounts-payable.controller';
 import { AccountsPayableService } from './accounts-payable.service';
 import { AccountsPayableReversalsService } from './accounts-payable-reversals.service';
+import { AccountsPayableCreditAnalyticsService } from './accounts-payable-credit-analytics.service';
 
 @Module({
   controllers: [AccountsPayableController],
-  providers: [AccountsPayableService, AccountsPayableReversalsService],
-  exports: [AccountsPayableService, AccountsPayableReversalsService],
+  providers: [AccountsPayableService, AccountsPayableReversalsService, AccountsPayableCreditAnalyticsService],
+  exports: [AccountsPayableService, AccountsPayableReversalsService, AccountsPayableCreditAnalyticsService],
 })
 export class AccountsPayableModule {}
