1ea19eb5c30609520f64feb508a5d1b196fc9168
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-10 11:20:46 +0300

    feat(accounting): register supplier payment reversal service

--- apps/api/src/modules/accounts-payable/accounts-payable.module.ts
+++ apps/api/src/modules/accounts-payable/accounts-payable.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AccountsPayableController } from './accounts-payable.controller';
 import { AccountsPayableService } from './accounts-payable.service';
+import { AccountsPayableReversalsService } from './accounts-payable-reversals.service';
 
 @Module({
   controllers: [AccountsPayableController],
-  providers: [AccountsPayableService],
-  exports: [AccountsPayableService],
+  providers: [AccountsPayableService, AccountsPayableReversalsService],
+  exports: [AccountsPayableService, AccountsPayableReversalsService],
 })
 export class AccountsPayableModule {}
