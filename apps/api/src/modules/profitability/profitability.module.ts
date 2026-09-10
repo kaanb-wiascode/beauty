02967c7e7531a1b2579ad1985f138f63f4890d50
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-10 11:41:51 +0300

    feat(reporting): register cost center service

--- apps/api/src/modules/profitability/profitability.module.ts
+++ apps/api/src/modules/profitability/profitability.module.ts
@@ -3,10 +3,11 @@ import { ProfitabilityController } from './profitability.controller';
 import { ProfitabilityService } from './profitability.service';
 import { NetProfitabilityService } from './net-profitability.service';
 import { ProfitabilityConfigService } from './profitability-config.service';
+import { CostCenterService } from './cost-center.service';
 
 @Module({
   controllers: [ProfitabilityController],
-  providers: [ProfitabilityService, NetProfitabilityService, ProfitabilityConfigService],
-  exports: [ProfitabilityService, NetProfitabilityService, ProfitabilityConfigService],
+  providers: [ProfitabilityService, NetProfitabilityService, ProfitabilityConfigService, CostCenterService],
+  exports: [ProfitabilityService, NetProfitabilityService, ProfitabilityConfigService, CostCenterService],
 })
 export class ProfitabilityModule {}
