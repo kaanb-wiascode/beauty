b53c87f4044feafdb88588e00a7e8b6ea4edaae8
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-11 13:50:47 +0300

    feat(hr): register payroll settlement reporting

--- apps/api/src/modules/hr/hr.module.ts
+++ apps/api/src/modules/hr/hr.module.ts
@@ -3,10 +3,12 @@ import { HrController } from './hr.controller';
 import { HrService } from './hr.service';
 import { PayrollAccountingService } from './payroll-accounting.service';
 import { PayrollPeriodService } from './payroll-period.service';
+import { PayrollSettlementService } from './payroll-settlement.service';
+import { PayrollReportService } from './payroll-report.service';
 
 @Module({
   controllers: [HrController],
-  providers: [HrService, PayrollAccountingService, PayrollPeriodService],
-  exports: [HrService, PayrollAccountingService, PayrollPeriodService],
+  providers: [HrService, PayrollAccountingService, PayrollPeriodService, PayrollSettlementService, PayrollReportService],
+  exports: [HrService, PayrollAccountingService, PayrollPeriodService, PayrollSettlementService, PayrollReportService],
 })
 export class HrModule {}
