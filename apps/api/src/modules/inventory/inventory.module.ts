961877e389919fde0aa5091a5b6d2def9c1b2a2e
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-11 13:35:19 +0300

    feat(inventory): register governance services

--- apps/api/src/modules/inventory/inventory.module.ts
+++ apps/api/src/modules/inventory/inventory.module.ts
@@ -2,10 +2,25 @@ import { Module } from '@nestjs/common';
 import { InventoryController } from './inventory.controller';
 import { InventoryService } from './inventory.service';
 import { WarehouseAccountingService } from './warehouse-accounting.service';
+import { InventoryGovernanceService } from './inventory-governance.service';
+import { InventoryTransferReceiptService } from './inventory-transfer-receipt.service';
+import { InventoryValuationReportService } from './inventory-valuation-report.service';
 
 @Module({
   controllers: [InventoryController],
-  providers: [InventoryService, WarehouseAccountingService],
-  exports: [InventoryService, WarehouseAccountingService],
+  providers: [
+    InventoryService,
+    WarehouseAccountingService,
+    InventoryGovernanceService,
+    InventoryTransferReceiptService,
+    InventoryValuationReportService,
+  ],
+  exports: [
+    InventoryService,
+    WarehouseAccountingService,
+    InventoryGovernanceService,
+    InventoryTransferReceiptService,
+    InventoryValuationReportService,
+  ],
 })
 export class InventoryModule {}
