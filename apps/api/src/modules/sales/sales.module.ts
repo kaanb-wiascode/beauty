2f9634a116459581968ed03f8e207f05f093b15f
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-11 12:06:23 +0300

    feat(tax): make tax service available to sales

--- apps/api/src/modules/sales/sales.module.ts
+++ apps/api/src/modules/sales/sales.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AccountingModule } from '../accounting/accounting.module';
 import { InstallmentsModule } from '../installments/installments.module';
+import { TaxModule } from '../tax/tax.module';
 import { SalesController } from './sales.controller';
 import { SalesService } from './sales.service';
 
 @Module({
-  imports: [InstallmentsModule, AccountingModule],
+  imports: [InstallmentsModule, AccountingModule, TaxModule],
   controllers: [SalesController],
   providers: [SalesService],
   exports: [SalesService],
