c280589c8fbd094c7712d9af5e124acfc691bd8f
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-11 13:44:35 +0300

    feat(hr): register payroll period service

--- apps/api/src/modules/hr/hr.module.ts
+++ apps/api/src/modules/hr/hr.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { HrController } from './hr.controller';
 import { HrService } from './hr.service';
 import { PayrollAccountingService } from './payroll-accounting.service';
+import { PayrollPeriodService } from './payroll-period.service';
 
 @Module({
   controllers: [HrController],
-  providers: [HrService, PayrollAccountingService],
-  exports: [HrService, PayrollAccountingService],
+  providers: [HrService, PayrollAccountingService, PayrollPeriodService],
+  exports: [HrService, PayrollAccountingService, PayrollPeriodService],
 })
 export class HrModule {}
