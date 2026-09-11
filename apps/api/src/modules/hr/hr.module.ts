95bee8e3a4cb0bd59f9e8a5915b73b47587fb921
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-11 14:03:25 +0300

    feat(hr): register payroll governance services

--- apps/api/src/modules/hr/hr.module.ts
+++ apps/api/src/modules/hr/hr.module.ts
@@ -5,10 +5,34 @@ import { PayrollAccountingService } from './payroll-accounting.service';
 import { PayrollPeriodService } from './payroll-period.service';
 import { PayrollSettlementService } from './payroll-settlement.service';
 import { PayrollReportService } from './payroll-report.service';
+import { PayrollCostCenterAccountingService } from './payroll-cost-center-accounting.service';
+import { PayrollPostingOrchestratorService } from './payroll-posting-orchestrator.service';
+import { PayrollReversalService } from './payroll-reversal.service';
+import { PayrollDashboardService } from './payroll-dashboard.service';
 
 @Module({
   controllers: [HrController],
-  providers: [HrService, PayrollAccountingService, PayrollPeriodService, PayrollSettlementService, PayrollReportService],
-  exports: [HrService, PayrollAccountingService, PayrollPeriodService, PayrollSettlementService, PayrollReportService],
+  providers: [
+    HrService,
+    PayrollAccountingService,
+    PayrollPeriodService,
+    PayrollSettlementService,
+    PayrollReportService,
+    PayrollCostCenterAccountingService,
+    PayrollPostingOrchestratorService,
+    PayrollReversalService,
+    PayrollDashboardService,
+  ],
+  exports: [
+    HrService,
+    PayrollAccountingService,
+    PayrollPeriodService,
+    PayrollSettlementService,
+    PayrollReportService,
+    PayrollCostCenterAccountingService,
+    PayrollPostingOrchestratorService,
+    PayrollReversalService,
+    PayrollDashboardService,
+  ],
 })
 export class HrModule {}
