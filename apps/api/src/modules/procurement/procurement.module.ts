0622eee8eb8ea22e93bee92f06d9136c4b0b4f92
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-12 08:10:52 +0300

    feat(procurement): register receipt query surface

--- apps/api/src/modules/procurement/procurement.module.ts
+++ apps/api/src/modules/procurement/procurement.module.ts
@@ -7,9 +7,11 @@ import { ProcurementReturnsService } from './procurement-returns.service';
 import { ProcurementReturnRequestsService } from './procurement-return-requests.service';
 import { ProcurementReplacementsService } from './procurement-replacements.service';
 import { ProcurementOrdersQueryService } from './procurement-orders-query.service';
+import { ProcurementReceiptQueryService } from './procurement-receipt-query.service';
+import { ProcurementReceiptQueryController } from './procurement-receipt-query.controller';
 
 @Module({
-  controllers: [ProcurementController],
+  controllers: [ProcurementController, ProcurementReceiptQueryController],
   providers: [
     ProcurementService,
     ProcurementRequestsService,
@@ -18,6 +20,7 @@ import { ProcurementOrdersQueryService } from './procurement-orders-query.servic
     ProcurementReturnRequestsService,
     ProcurementReplacementsService,
     ProcurementOrdersQueryService,
+    ProcurementReceiptQueryService,
   ],
   exports: [
     ProcurementService,
@@ -27,6 +30,7 @@ import { ProcurementOrdersQueryService } from './procurement-orders-query.servic
     ProcurementReturnRequestsService,
     ProcurementReplacementsService,
     ProcurementOrdersQueryService,
+    ProcurementReceiptQueryService,
   ],
 })
 export class ProcurementModule {}
