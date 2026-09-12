09309e2c0807697e984d278c445126e0911806bf
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-12 17:01:58 +0300

    feat(marketplace): register booking orchestration

--- apps/api/src/modules/marketplace/marketplace.module.ts
+++ apps/api/src/modules/marketplace/marketplace.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { MarketplaceBookingService } from './marketplace-booking.service';
 import { MarketplaceController } from './marketplace.controller';
 import { MarketplacePublicController } from './marketplace-public.controller';
 import { MarketplaceService } from './marketplace.service';
 
 @Module({
   controllers: [MarketplaceController, MarketplacePublicController],
-  providers: [MarketplaceService],
+  providers: [MarketplaceService, MarketplaceBookingService],
 })
 export class MarketplaceModule {}
