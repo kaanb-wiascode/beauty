f354490b9b5d08d941d38c5eba079bb9a63da006
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-12 05:13:49 +0300

    feat(training): wire managed lesson document storage

--- apps/api/src/modules/training/training.module.ts
+++ apps/api/src/modules/training/training.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ObjectStorageModule } from '../../common/storage/object-storage.module';
 import { CompetencyController } from './competency.controller';
 import { CompetencyService } from './competency.service';
 import { CompetencyReviewController } from './competency-review.controller';
@@ -17,6 +18,8 @@ import { TrainingCertificateController } from './training-certificate.controller
 import { TrainingCertificateService } from './training-certificate.service';
 import { TrainingCompetencyBridgeController } from './training-competency-bridge.controller';
 import { TrainingCompetencyBridgeService } from './training-competency-bridge.service';
+import { TrainingContentStorageController } from './training-content-storage.controller';
+import { TrainingContentStorageService } from './training-content-storage.service';
 import { TrainingEffectivenessController } from './training-effectiveness.controller';
 import { TrainingEffectivenessService } from './training-effectiveness.service';
 import { TrainingLessonProgressController } from './training-lesson-progress.controller';
@@ -31,8 +34,9 @@ import { TrainingController } from './training.controller';
 import { TrainingService } from './training.service';
 
 @Module({
-  controllers:[TrainingController,TrainingAnalyticsController,TrainingBranchAnalyticsController,TrainingQuestionBankController,CompetencyController,CompetencyReviewController,CompetencyTrainingController,LmsController,PositionCompetencyController,TrainingCertificateController,TrainingCompetencyBridgeController,TrainingEffectivenessController,TrainingLessonProgressController,TrainingPlanningController,TrainingProgramController],
-  providers:[TrainingService,TrainingAnalyticsService,TrainingBranchAnalyticsService,TrainingQuestionBankService,CompetencyService,CompetencyReviewService,CompetencyTrainingService,LmsService,PositionCompetencyService,TrainingCertificateService,TrainingCompetencyBridgeService,TrainingEffectivenessService,TrainingLessonProgressService,TrainingPlanningService,TrainingProgramService],
-  exports:[TrainingService,TrainingAnalyticsService,TrainingBranchAnalyticsService,TrainingQuestionBankService,CompetencyService,CompetencyReviewService,CompetencyTrainingService,LmsService,PositionCompetencyService,TrainingCertificateService,TrainingCompetencyBridgeService,TrainingEffectivenessService,TrainingLessonProgressService,TrainingPlanningService,TrainingProgramService],
+  imports: [ObjectStorageModule],
+  controllers:[TrainingController,TrainingAnalyticsController,TrainingBranchAnalyticsController,TrainingQuestionBankController,TrainingContentStorageController,CompetencyController,CompetencyReviewController,CompetencyTrainingController,LmsController,PositionCompetencyController,TrainingCertificateController,TrainingCompetencyBridgeController,TrainingEffectivenessController,TrainingLessonProgressController,TrainingPlanningController,TrainingProgramController],
+  providers:[TrainingService,TrainingAnalyticsService,TrainingBranchAnalyticsService,TrainingQuestionBankService,TrainingContentStorageService,CompetencyService,CompetencyReviewService,CompetencyTrainingService,LmsService,PositionCompetencyService,TrainingCertificateService,TrainingCompetencyBridgeService,TrainingEffectivenessService,TrainingLessonProgressService,TrainingPlanningService,TrainingProgramService],
+  exports:[TrainingService,TrainingAnalyticsService,TrainingBranchAnalyticsService,TrainingQuestionBankService,TrainingContentStorageService,CompetencyService,CompetencyReviewService,CompetencyTrainingService,LmsService,PositionCompetencyService,TrainingCertificateService,TrainingCompetencyBridgeService,TrainingEffectivenessService,TrainingLessonProgressService,TrainingPlanningService,TrainingProgramService],
 })
 export class TrainingModule {}
