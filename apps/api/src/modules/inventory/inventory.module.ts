5e4acff26bb59d41ae4d6176edccc8fee0e90895
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-13 02:01:58 +0300

    feat(inventory): register lot management

--- apps/api/src/modules/inventory/inventory.module.ts
+++ apps/api/src/modules/inventory/inventory.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
 import { InventoryController } from './inventory.controller';
+import { InventoryLotsController } from './inventory-lots.controller';
 import { InventoryService } from './inventory.service';
+import { InventoryLotsService } from './inventory-lots.service';
 import { WarehouseAccountingService } from './warehouse-accounting.service';
 import { InventoryGovernanceService } from './inventory-governance.service';
 import { InventoryTransferReceiptService } from './inventory-transfer-receipt.service';
 import { InventoryValuationReportService } from './inventory-valuation-report.service';
 
 @Module({
-  controllers: [InventoryController],
+  controllers: [InventoryController, InventoryLotsController],
   providers: [
     InventoryService,
+    InventoryLotsService,
     WarehouseAccountingService,
     InventoryGovernanceService,
     InventoryTransferReceiptService,
@@ -17,6 +20,7 @@ import { InventoryValuationReportService } from './inventory-valuation-report.se
   ],
   exports: [
     InventoryService,
+    InventoryLotsService,
     WarehouseAccountingService,
     InventoryGovernanceService,
     InventoryTransferReceiptService,
