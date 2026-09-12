83d8bbeb3017d50032d2dbb83f75a921831d70c6
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-12 03:38:54 +0300

    feat(training): wire certificate lifecycle

--- apps/api/src/modules/training/training.module.ts
+++ apps/api/src/modules/training/training.module.ts
@@ -5,6 +5,8 @@ import { CompetencyTrainingController } from './competency-training.controller';
 import { CompetencyTrainingService } from './competency-training.service';
 import { LmsController } from './lms.controller';
 import { LmsService } from './lms.service';
+import { TrainingCertificateController } from './training-certificate.controller';
+import { TrainingCertificateService } from './training-certificate.service';
 import { TrainingCompetencyBridgeController } from './training-competency-bridge.controller';
 import { TrainingCompetencyBridgeService } from './training-competency-bridge.service';
 import { TrainingEffectivenessController } from './training-effectiveness.controller';
@@ -15,8 +17,8 @@ import { TrainingController } from './training.controller';
 import { TrainingService } from './training.service';
 
 @Module({
-  controllers:[TrainingController,CompetencyController,CompetencyTrainingController,LmsController,TrainingCompetencyBridgeController,TrainingEffectivenessController,TrainingLessonProgressController],
-  providers:[TrainingService,CompetencyService,CompetencyTrainingService,LmsService,TrainingCompetencyBridgeService,TrainingEffectivenessService,TrainingLessonProgressService],
-  exports:[TrainingService,CompetencyService,CompetencyTrainingService,LmsService,TrainingCompetencyBridgeService,TrainingEffectivenessService,TrainingLessonProgressService],
+  controllers:[TrainingController,CompetencyController,CompetencyTrainingController,LmsController,TrainingCertificateController,TrainingCompetencyBridgeController,TrainingEffectivenessController,TrainingLessonProgressController],
+  providers:[TrainingService,CompetencyService,CompetencyTrainingService,LmsService,TrainingCertificateService,TrainingCompetencyBridgeService,TrainingEffectivenessService,TrainingLessonProgressService],
+  exports:[TrainingService,CompetencyService,CompetencyTrainingService,LmsService,TrainingCertificateService,TrainingCompetencyBridgeService,TrainingEffectivenessService,TrainingLessonProgressService],
 })
 export class TrainingModule {}
