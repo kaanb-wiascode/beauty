da8598ddf166e5e41b0d0c486521b2af5c467820
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-13 20:47:24 +0300

    feat(communications): register crm conversion service

--- apps/api/src/modules/corporate-communications/corporate-communications.module.ts
+++ apps/api/src/modules/corporate-communications/corporate-communications.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CorporateCommunicationsController } from './corporate-communications.controller';
 import { CorporateCommunicationsService } from './corporate-communications.service';
+import { MarketingLeadConversionService } from './marketing-lead-conversion.service';
 
 @Module({
   controllers: [CorporateCommunicationsController],
-  providers: [CorporateCommunicationsService],
-  exports: [CorporateCommunicationsService],
+  providers: [CorporateCommunicationsService, MarketingLeadConversionService],
+  exports: [CorporateCommunicationsService, MarketingLeadConversionService],
 })
 export class CorporateCommunicationsModule {}
