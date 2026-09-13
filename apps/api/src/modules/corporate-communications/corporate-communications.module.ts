c13b4ff5d1189601075a765851f95b0c3c4e7688
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-13 21:20:49 +0300

    feat(communications): register appointment orchestration

--- apps/api/src/modules/corporate-communications/corporate-communications.module.ts
+++ apps/api/src/modules/corporate-communications/corporate-communications.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { CorporateCommunicationsController } from './corporate-communications.controller';
 import { CorporateCommunicationsService } from './corporate-communications.service';
 import { MarketingLeadConversionController } from './marketing-lead-conversion.controller';
+import { MarketingLeadAppointmentService } from './marketing-lead-appointment.service';
 import { MarketingLeadCrmBridgeService } from './marketing-lead-crm-bridge.service';
 import { MarketingLeadCustomerBridgeService } from './marketing-lead-customer-bridge.service';
 
@@ -12,11 +13,13 @@ import { MarketingLeadCustomerBridgeService } from './marketing-lead-customer-br
   ],
   providers: [
     CorporateCommunicationsService,
+    MarketingLeadAppointmentService,
     MarketingLeadCrmBridgeService,
     MarketingLeadCustomerBridgeService,
   ],
   exports: [
     CorporateCommunicationsService,
+    MarketingLeadAppointmentService,
     MarketingLeadCrmBridgeService,
     MarketingLeadCustomerBridgeService,
   ],
