bdf028d9f0049be2cc2d45614d29c8ecc952209e
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-13 20:52:55 +0300

    fix(communications): register hardened crm bridge

--- apps/api/src/modules/corporate-communications/corporate-communications.module.ts
+++ apps/api/src/modules/corporate-communications/corporate-communications.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { CorporateCommunicationsController } from './corporate-communications.controller';
 import { CorporateCommunicationsService } from './corporate-communications.service';
 import { MarketingLeadConversionController } from './marketing-lead-conversion.controller';
-import { MarketingLeadConversionService } from './marketing-lead-conversion.service';
+import { MarketingLeadCrmBridgeService } from './marketing-lead-crm-bridge.service';
 
 @Module({
   controllers: [
     CorporateCommunicationsController,
     MarketingLeadConversionController,
   ],
-  providers: [CorporateCommunicationsService, MarketingLeadConversionService],
-  exports: [CorporateCommunicationsService, MarketingLeadConversionService],
+  providers: [CorporateCommunicationsService, MarketingLeadCrmBridgeService],
+  exports: [CorporateCommunicationsService, MarketingLeadCrmBridgeService],
 })
 export class CorporateCommunicationsModule {}
