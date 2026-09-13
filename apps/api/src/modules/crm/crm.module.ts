796c82b742efbb57a1479e8e31987242a682e1b0
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-13 23:46:19 +0300

    feat(crm): register opportunity creation service

--- apps/api/src/modules/crm/crm.module.ts
+++ apps/api/src/modules/crm/crm.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CrmController } from './crm.controller';
+import { CrmOpportunityService } from './crm-opportunity.service';
 import { CrmService } from './crm.service';
 
 @Module({
   controllers: [CrmController],
-  providers: [CrmService],
-  exports: [CrmService],
+  providers: [CrmService, CrmOpportunityService],
+  exports: [CrmService, CrmOpportunityService],
 })
 export class CrmModule {}
