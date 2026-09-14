2f819b5ce89170d6fef66ab304cb9d94fed6206e
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-14 03:01:13 +0300

    feat(crm): register commercial edit controller

--- apps/api/src/modules/crm/crm.module.ts
+++ apps/api/src/modules/crm/crm.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
+import { CrmCommercialController } from './crm-commercial.controller';
 import { CrmController } from './crm.controller';
 import { CrmOperationsController } from './crm-operations.controller';
 import { CrmOperationsService } from './crm-operations.service';
+import { CrmOpportunityCommercialService } from './crm-opportunity-commercial.service';
 import { CrmOpportunityService } from './crm-opportunity.service';
 import { CrmService } from './crm.service';
 
 @Module({
-  controllers: [CrmController, CrmOperationsController],
-  providers: [CrmService, CrmOpportunityService, CrmOperationsService],
+  controllers: [CrmController, CrmOperationsController, CrmCommercialController],
+  providers: [
+    CrmService,
+    CrmOpportunityService,
+    CrmOperationsService,
+    CrmOpportunityCommercialService,
+  ],
   exports: [CrmService, CrmOpportunityService, CrmOperationsService],
 })
 export class CrmModule {}
