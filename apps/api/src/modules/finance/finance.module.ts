582f07d315d167d5a190a40cbb155474cd0d3ac5
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-15 17:19:16 +0300

    feat(finance): wire expense accounting APIs

--- apps/api/src/modules/finance/finance.module.ts
+++ apps/api/src/modules/finance/finance.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ExpenseAccountingController } from './expense-accounting.controller';
+import { ExpenseAccountingService } from './expense-accounting.service';
 import { ExpensesController } from './expenses.controller';
 import { ExpensesService } from './expenses.service';
 import { FinanceSetupController } from './finance-setup.controller';
 import { FinanceSetupService } from './finance-setup.service';
 
 @Module({
-  controllers: [ExpensesController, FinanceSetupController],
-  providers: [ExpensesService, FinanceSetupService],
-  exports: [ExpensesService, FinanceSetupService],
+  controllers: [ExpensesController, FinanceSetupController, ExpenseAccountingController],
+  providers: [ExpensesService, FinanceSetupService, ExpenseAccountingService],
+  exports: [ExpensesService, FinanceSetupService, ExpenseAccountingService],
 })
 export class FinanceModule {}
