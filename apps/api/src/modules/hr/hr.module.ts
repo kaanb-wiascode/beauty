b21b5653aeb348ade68875da757c6f455baa77fd
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-15 17:10:33 +0300

    feat(hr): register organization service

--- apps/api/src/modules/hr/hr.module.ts
+++ apps/api/src/modules/hr/hr.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { HrController } from './hr.controller';
 import { HrService } from './hr.service';
+import { HrOrganizationService } from './hr-organization.service';
 import { PayrollAccountingService } from './payroll-accounting.service';
 import { PayrollPeriodService } from './payroll-period.service';
 import { PayrollSettlementService } from './payroll-settlement.service';
@@ -18,6 +19,7 @@ import { PayrollPolicyService } from './payroll-policy.service';
   controllers: [HrController],
   providers: [
     HrService,
+    HrOrganizationService,
     PayrollAccountingService,
     PayrollPeriodService,
     PayrollSettlementService,
@@ -33,6 +35,7 @@ import { PayrollPolicyService } from './payroll-policy.service';
   ],
   exports: [
     HrService,
+    HrOrganizationService,
     PayrollAccountingService,
     PayrollPeriodService,
     PayrollSettlementService,
