64ca2d3af7e4937717e689110f4d1257681d0b7b
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-15 22:40:31 +0300

    feat(admin): wire business policy into payments

--- apps/api/src/modules/payments/payments.module.ts
+++ apps/api/src/modules/payments/payments.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { BusinessPolicyModule } from '../business-policies/business-policy.module';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 
 @Module({
+  imports: [BusinessPolicyModule],
   controllers: [PaymentsController],
   providers: [PaymentsService],
   exports: [PaymentsService],
