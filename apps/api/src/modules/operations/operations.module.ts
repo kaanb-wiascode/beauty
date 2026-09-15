6e71c11604005302e603317812e3ac5889509dc6
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-15 18:54:49 +0300

    feat(operations): register service execution lifecycle

--- apps/api/src/modules/operations/operations.module.ts
+++ apps/api/src/modules/operations/operations.module.ts
@@ -4,13 +4,16 @@ import { OperationsAllocationService } from './operations-allocation.service';
 import { OperationsCapacityService } from './operations-capacity.service';
 import { OperationsResourcesController } from './operations-resources.controller';
 import { OperationsResourcesService } from './operations-resources.service';
+import { ServiceExecutionsController } from './service-executions.controller';
+import { ServiceExecutionsService } from './service-executions.service';
 
 @Module({
-  controllers: [OperationsResourcesController],
+  controllers: [OperationsResourcesController, ServiceExecutionsController],
   providers: [
     OperationsResourcesService,
     OperationsAllocationService,
     OperationsCapacityService,
+    ServiceExecutionsService,
   ],
 })
 export class OperationsModule {}
