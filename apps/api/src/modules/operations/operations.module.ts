ae7be9521e5bd9437deab8098aa57156dadfd24e
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-15 19:05:39 +0300

    feat(operations): register resource block lifecycle

--- apps/api/src/modules/operations/operations.module.ts
+++ apps/api/src/modules/operations/operations.module.ts
@@ -2,17 +2,24 @@ import { Module } from '@nestjs/common';
 
 import { OperationsAllocationService } from './operations-allocation.service';
 import { OperationsCapacityService } from './operations-capacity.service';
+import { OperationsResourceBlocksController } from './operations-resource-blocks.controller';
+import { OperationsResourceBlocksService } from './operations-resource-blocks.service';
 import { OperationsResourcesController } from './operations-resources.controller';
 import { OperationsResourcesService } from './operations-resources.service';
 import { ServiceExecutionsController } from './service-executions.controller';
 import { ServiceExecutionsService } from './service-executions.service';
 
 @Module({
-  controllers: [OperationsResourcesController, ServiceExecutionsController],
+  controllers: [
+    OperationsResourcesController,
+    OperationsResourceBlocksController,
+    ServiceExecutionsController,
+  ],
   providers: [
     OperationsResourcesService,
     OperationsAllocationService,
     OperationsCapacityService,
+    OperationsResourceBlocksService,
     ServiceExecutionsService,
   ],
 })
