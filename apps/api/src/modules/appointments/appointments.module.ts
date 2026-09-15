b3f3bcab74cf37205ab89891b4c4fa615b036673
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-15 21:08:42 +0300

    refactor(appointments): export appointment service

--- apps/api/src/modules/appointments/appointments.module.ts
+++ apps/api/src/modules/appointments/appointments.module.ts
@@ -6,5 +6,6 @@ import { AppointmentsService } from './appointments.service';
 @Module({
   controllers: [AppointmentsController],
   providers: [AppointmentsService],
+  exports: [AppointmentsService],
 })
 export class AppointmentsModule {}
