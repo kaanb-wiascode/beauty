7f4d1a2aaf859e4fb858888fc9852df6cbe1a72b
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-15 17:16:21 +0300

    feat(finance): register finance setup endpoints

--- apps/api/src/modules/finance/finance.module.ts
+++ apps/api/src/modules/finance/finance.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ExpensesController } from './expenses.controller';
 import { ExpensesService } from './expenses.service';
+import { FinanceSetupController } from './finance-setup.controller';
+import { FinanceSetupService } from './finance-setup.service';
 
 @Module({
-  controllers: [ExpensesController],
-  providers: [ExpensesService],
-  exports: [ExpensesService],
+  controllers: [ExpensesController, FinanceSetupController],
+  providers: [ExpensesService, FinanceSetupService],
+  exports: [ExpensesService, FinanceSetupService],
 })
 export class FinanceModule {}
