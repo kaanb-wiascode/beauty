c3dc6eb444ecc400f7b8dcb8b9e548a69cb3e158
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-15 18:48:57 +0300

    feat(operations): register capacity service

--- apps/api/src/modules/operations/operations.module.ts
+++ apps/api/src/modules/operations/operations.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 
 import { OperationsAllocationService } from './operations-allocation.service';
+import { OperationsCapacityService } from './operations-capacity.service';
 import { OperationsResourcesController } from './operations-resources.controller';
 import { OperationsResourcesService } from './operations-resources.service';
 
 @Module({
   controllers: [OperationsResourcesController],
-  providers: [OperationsResourcesService, OperationsAllocationService],
+  providers: [
+    OperationsResourcesService,
+    OperationsAllocationService,
+    OperationsCapacityService,
+  ],
 })
 export class OperationsModule {}
