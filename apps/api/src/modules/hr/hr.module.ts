a2b4a1593a34539e34b88f909824d5a6edee0d5d
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-15 17:25:43 +0300

    feat(hr): register onboarding service

--- apps/api/src/modules/hr/hr.module.ts
+++ apps/api/src/modules/hr/hr.module.ts
@@ -4,17 +4,6 @@ import { HrService } from './hr.service';
 import { HrOrganizationService } from './hr-organization.service';
 import { Employee360Service } from './employee-360.service';
 import { EmploymentHistoryService } from './employment-history.service';
-import { PayrollAccountingService } from './payroll-accounting.service';
-import { PayrollPeriodService } from './payroll-period.service';
-import { PayrollSettlementService } from './payroll-settlement.service';
-import { PayrollReportService } from './payroll-report.service';
-import { PayrollCostCenterAccountingService } from './payroll-cost-center-accounting.service';
-import { PayrollPostingOrchestratorService } from './payroll-posting-orchestrator.service';
-import { PayrollReversalService } from './payroll-reversal.service';
-import { PayrollDashboardService } from './payroll-dashboard.service';
-import { PayrollPaymentReversalService } from './payroll-payment-reversal.service';
-import { PayrollWorkInputService } from './payroll-work-input.service';
-import { HrAnalyticsService } from './hr-analytics.service';
-import { PayrollPolicyService } from './payroll-policy.service';
-@Module({controllers:[HrController],providers:[HrService,HrOrganizationService,Employee360Service,EmploymentHistoryService,PayrollAccountingService,PayrollPeriodService,PayrollSettlementService,PayrollReportService,PayrollCostCenterAccountingService,PayrollPostingOrchestratorService,PayrollReversalService,PayrollDashboardService,PayrollPaymentReversalService,PayrollWorkInputService,HrAnalyticsService,PayrollPolicyService],exports:[HrService,HrOrganizationService,Employee360Service,EmploymentHistoryService,PayrollAccountingService,PayrollPeriodService,PayrollSettlementService,PayrollReportService,PayrollCostCenterAccountingService,PayrollPostingOrchestratorService,PayrollReversalService,PayrollDashboardService,PayrollPaymentReversalService,PayrollWorkInputService,HrAnalyticsService,PayrollPolicyService]})
-export class HrModule {}
+import { OnboardingService } from './onboarding.service';
+import { PayrollAccountingService } from './payroll-accounting.service';import { PayrollPeriodService } from './payroll-period.service';import { PayrollSettlementService } from './payroll-settlement.service';import { PayrollReportService } from './payroll-report.service';import { PayrollCostCenterAccountingService } from './payroll-cost-center-accounting.service';import { PayrollPostingOrchestratorService } from './payroll-posting-orchestrator.service';import { PayrollReversalService } from './payroll-reversal.service';import { PayrollDashboardService } from './payroll-dashboard.service';import { PayrollPaymentReversalService } from './payroll-payment-reversal.service';import { PayrollWorkInputService } from './payroll-work-input.service';import { HrAnalyticsService } from './hr-analytics.service';import { PayrollPolicyService } from './payroll-policy.service';
+@Module({controllers:[HrController],providers:[HrService,HrOrganizationService,Employee360Service,EmploymentHistoryService,OnboardingService,PayrollAccountingService,PayrollPeriodService,PayrollSettlementService,PayrollReportService,PayrollCostCenterAccountingService,PayrollPostingOrchestratorService,PayrollReversalService,PayrollDashboardService,PayrollPaymentReversalService,PayrollWorkInputService,HrAnalyticsService,PayrollPolicyService],exports:[HrService,HrOrganizationService,Employee360Service,EmploymentHistoryService,OnboardingService,PayrollAccountingService,PayrollPeriodService,PayrollSettlementService,PayrollReportService,PayrollCostCenterAccountingService,PayrollPostingOrchestratorService,PayrollReversalService,PayrollDashboardService,PayrollPaymentReversalService,PayrollWorkInputService,HrAnalyticsService,PayrollPolicyService]})export class HrModule{}
