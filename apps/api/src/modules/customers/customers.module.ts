20c15cd34a691100dcefa205303ead0c5ea5f175
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-15 21:58:20 +0300

    feat(customers): export reporting query service

--- apps/api/src/modules/customers/customers.module.ts
+++ apps/api/src/modules/customers/customers.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 
 import { CustomersController } from "./customers.controller";
 import { CustomersService } from "./customers.service";
+import { CustomerReportingService } from "./customer-reporting.service";
 import { CareEventsController } from "./care-events.controller";
 import { CareEventsService } from "./care-events.service";
 
@@ -12,7 +13,9 @@ import { CareEventsService } from "./care-events.service";
   ],
   providers: [
     CustomersService,
+    CustomerReportingService,
     CareEventsService,
   ],
+  exports: [CustomerReportingService],
 })
 export class CustomersModule {}
