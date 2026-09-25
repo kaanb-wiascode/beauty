e4e1726be6c03cd8b8c964dcd157839e55e52e83
Kaan Burak ATAMER <kaanatamer@wiascorp.com>
2026-09-25 03:11:25 +0300

    feat(team): connect private object storage

--- apps/api/src/modules/team/team.module.ts
+++ apps/api/src/modules/team/team.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ObjectStorageModule } from '../../common/storage/object-storage.module';
 import { TeamController } from './team.controller';
 import { TeamService } from './team.service';
 
 @Module({
+  imports: [ObjectStorageModule],
   controllers: [TeamController],
   providers: [TeamService],
   exports: [TeamService],
